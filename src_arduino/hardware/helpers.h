037345f14268581072695b5cae19b15b95e03b70
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-19 00:42:49 -0300

    Agora temos build date/time A mensagem será quando em DEBUG, na inicialização da máquina

--- src_arduino/hardware/helpers.h
+++ src_arduino/hardware/helpers.h
@@ -11,6 +11,7 @@ class Helpers {
 public:
   static void lcdWriteBottomMenu(uint8_t opt = HELPERS_OK_BTN | HELPERS_CANCEL_BTN);
   static void lcdWrite(uint8_t, uint8_t, const __FlashStringHelper *ifsh);
+  static void lcdWriteBuildTime();
 };
 
 #endif /* Helpers_H */
--- src_arduino/helpers.cpp
+++ src_arduino/helpers.cpp
@@ -1,6 +1,8 @@
 #include "hardware/helpers.h"
 #include "globals.h"
 
+#define DEBUG
+
 void Helpers::lcdWriteBottomMenu(uint8_t opt) {  
   Helpers::lcdWrite(0, 2, F("--------------------"));
   
@@ -14,4 +16,13 @@ void Helpers::lcdWriteBottomMenu(uint8_t opt) {
 void Helpers::lcdWrite(uint8_t col, uint8_t line, const __FlashStringHelper *ifsh) {
   gGlobals.gLcd.setCursor(col, line);
   gGlobals.gLcd.print(ifsh);
+}
+
+void Helpers::lcdWriteBuildTime() {
+#ifdef DEBUG
+  lcdWrite(9, 2, F(__DATE__));
+  lcdWrite(12, 3, F(__TIME__));
+  lcdWrite(0, 3, F("ID:"));
+  gGlobals.gLcd.print(gGlobals.gMachineId);
+#endif
 }
