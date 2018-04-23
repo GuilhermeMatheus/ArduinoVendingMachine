a76b5ab6c77bbbfebba5be1cf52b0750631a1b4c
Guilherme Matheus Costa <costa.guimatheus@gmail.com>
2018-04-22 22:06:48 -0300

    Criação de mensagem no stateActivateChamber

--- src_arduino/stateActivateChamber.cpp
+++ src_arduino/stateActivateChamber.cpp
@@ -1,15 +1,23 @@
 #include "states/stateActivateChamber.h"
 #include <Arduino.h>
+#include "hardware/helpers.h"
 #include "globals.h"
 
 StateActivateChamber::StateActivateChamber(State *pNextState) : State(pNextState) {
-
 }
 
+//   ____________________
+// 0| Saldo restante: 
+// 0| R$120,00
+// 0|--------------------
+// 0|     Sirva-se
 void StateActivateChamber::enter() {
   gGlobals.gLcd.clear();
-  gGlobals.gLcd.setCursor(5, 1);
-  gGlobals.gLcd.print(F(" sirva-se"));
+  Helpers::lcdWrite(1, 0, F("Saldo restante"));
+  Helpers::lcdWrite(1, 1, F("R$ "));
+  gGlobals.gLcd.print(gGlobals.gSaleResponse.creditAfterTransaction);
+  Helpers::lcdWrite(0, 2, F("--------------------"));
+  Helpers::lcdWrite(5, 3, F("Sirva-se"));
 
   gGlobals.gChamber.activateHelix(gGlobals.gCurProd.helix);
 
